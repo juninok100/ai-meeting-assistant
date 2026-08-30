05062bde3d1eb9902825575a2cfb060e36fc7c25
juninok100 <juninok100@gmail.com>
2026-08-30 14:07:35 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="AI 회의 비서",
+    page_icon="🎙️"
+)
+
+st.title("🎙️ AI 회의 비서")
+
+st.write("녹음파일을 업로드하면 AI가 내용을 분석해주는 앱입니다.")
+
+st.success("앱이 정상적으로 실행되었습니다.")
